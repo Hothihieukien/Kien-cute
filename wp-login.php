1f2551c82cf50c6fc985b3d231bd5ce3ed5dbb8e
Hothihieukien <85327315+Hothihieukien@users.noreply.github.com>
2021-07-05 08:55:12 +0700

    KIENCT

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://hothihieukien.github.io/Kien-cute/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"afbd16ab2f"};
+var userProfileL10n = {"user_id":"0","nonce":"60e7e0d93f"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
